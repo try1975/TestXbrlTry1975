29c7fea63800d6701ffddc095befae502b01fc61
try1975 <try1975@mail.ru>
2020-11-21 12:56:19 +0300

    refactore

--- Program.cs
+++ Program.cs
@@ -9,32 +9,35 @@ namespace TestXbrlTry1975
     {
         static void Main(string[] args)
         {
+            #region Fields
+            var taskColor = ConsoleColor.Green;
+            var errorColor = ConsoleColor.Red;
+
+            var report1Path = "report1.xbrl";
+            var report2Path = "report2.xbrl";
+            Instance report1;
+            Instance report2; 
+            #endregion
+
             // для работы с XBRL решил использовать пакет Diwen.Xbrl (исходники https://github.com/dgm9704/Xoxo )
             // посмотрел реализацию, используется XmlReader для потокового чтения больших файлов,
             // реализованы интерфейсы IEquatable. Я сам бы так сделал, если бы был глубоко погружен в вопрос
 
             #region read files
-            var report1Path = "report1.xbrl";
-            var report2Path = "report2.xbrl";
-
-            Instance report1;
             try
             {
-
                 using var stream = new FileStream(report1Path, FileMode.Open, FileAccess.Read);
                 report1 = Instance.FromStream(stream: stream, removeUnusedObjects: false, collapseDuplicateContexts: false);
             }
             catch (Exception exception)
             {
-
-                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = errorColor;
                 Console.WriteLine($"Ошибка файла {report1Path}");
                 Console.WriteLine(exception.Message);
                 Console.WriteLine(exception.InnerException.Message);
                 return;
             }
 
-            Instance report2;
             try
             {
                 using var stream = new FileStream(report2Path, FileMode.Open, FileAccess.Read);
@@ -42,8 +45,7 @@ namespace TestXbrlTry1975
             }
             catch (Exception exception)
             {
-
-                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = errorColor;
                 Console.WriteLine($"Ошибка файла {report2Path}");
                 Console.WriteLine(exception.Message);
                 Console.WriteLine(exception.InnerException.Message);
@@ -52,7 +54,7 @@ namespace TestXbrlTry1975
             #endregion
 
             #region check contexts
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = taskColor;
             Console.WriteLine("Найти ошибки в файле (повторяющиеся контексты).");
             Console.ResetColor();
             FindDuplicates(report1Path, report1);
@@ -60,23 +62,25 @@ namespace TestXbrlTry1975
             #endregion
 
             #region compare facts
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("----------------------------------------------------");
+            Console.ForegroundColor = taskColor;
+            ConsoleWriteDevider();
             Console.WriteLine("Выявить различия: список отсутствующих и новые факты, факты с различающимися значениями.");
             var comparisonReport = InstanceComparer.Report(report1, report2);
             Console.ResetColor();
             foreach (var message in comparisonReport.Messages)
             {
                 Console.WriteLine($"{message}");
-            } 
+            }
             #endregion
 
+            #region Like XPath
             ////Написать запросы XPath для получения:
-            LinqAsXPath(report1Path, report1);
+            LinqAsXPath(report1Path, report1, taskColor); 
+            #endregion
 
             #region union files
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("----------------------------------------------------");
+            Console.ForegroundColor = taskColor;
+            ConsoleWriteDevider();
             Console.WriteLine("Объединить отчеты, на выходе получить новый объединенный отчет (xbrl) с объединенными списками контекстов, единиц измерений и значений (фактов).");
             Console.ResetColor();
             var unionPath = "unionReport.xbrl";
@@ -90,6 +94,11 @@ namespace TestXbrlTry1975
 
         }
 
+        /// <summary>
+        /// Find and remove dublicate Context
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="instance"></param>
         private static void FindDuplicates(string path, Instance instance)
         {
             var duplicates = instance.Contexts
@@ -121,12 +130,11 @@ namespace TestXbrlTry1975
         /// Use Linq as eqvivalent of XPath
         /// </summary>
         /// <param name="path"></param>
-        private static void LinqAsXPath(string path, Instance instance)
+        private static void LinqAsXPath(string path, Instance instance, ConsoleColor taskColor)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("----------------------------------------------------");
+            Console.ForegroundColor = taskColor;
+            ConsoleWriteDevider();
             var date = new DateTime(year: 2019, month: 4, day: 30);
-            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{path} - контексты с периодом xbrli:period/xbrli:instant, равным \"{date:yyyy-MM-dd}\"");
             Console.ResetColor();
             //Console.WriteLine("эквивалент XPath xbrli:period/xbrli:instant , равным \"2019-04-30\"");
@@ -135,8 +143,8 @@ namespace TestXbrlTry1975
                 .Select(z => z.Id)
                 ));
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("----------------------------------------------------");
+            Console.ForegroundColor = taskColor;
+            ConsoleWriteDevider();
             var dimensionName = "dim-int:ID_sobstv_CZBTaxis";
             Console.WriteLine($"{path} - контексты со сценарием, использующим измерение dimension=\"{dimensionName}\"");
             Console.ResetColor();
@@ -145,8 +153,8 @@ namespace TestXbrlTry1975
                 .Select(z => z.Id)
                 ));
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("----------------------------------------------------");
+            Console.ForegroundColor = taskColor;
+            ConsoleWriteDevider();
             Console.WriteLine($"{path} - контексты без сценария");
             Console.ResetColor();
             Console.WriteLine(string.Join(", ",
@@ -154,5 +162,7 @@ namespace TestXbrlTry1975
                 .Select(z => z.Id)
                 ));
         }
+
+        private static void ConsoleWriteDevider() => Console.WriteLine("----------------------------------------------------");
     }
 }
